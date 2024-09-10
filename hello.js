ebae0a736dbe4ed8477e24c47242d4eba264e556
noaGolWix <157351223+noaGolWix@users.noreply.github.com>
2024-09-10 13:45:37 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,18 @@
-class OneDialog extends HTMLElement {
+class MyCustomElement extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <div id="box" class="red-box">Hi Noa</div>
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>My Custom Element</title>
+      </head>
+      <body>
+        <h1>Hello from my custom element!</h1>
+        <p>This is some content within the custom element.</p>
+      </body>
+      </html>
     `;
   }
 }
 
-customElements.define('one-dialog', OneDialog);
+customElements.define('my-custom-element', MyCustomElement);
