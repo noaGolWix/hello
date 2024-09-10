7c1056c123aee661ba18b4b801656d8ac542803c
noaGolWix <157351223+noaGolWix@users.noreply.github.com>
2024-09-10 16:21:25 +0300

    Create custom-element-green.js

---
+++ custom-element-green.js
@@ -0,0 +1,24 @@
+class RedBox extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="box">
+        Hi Noa
+      </div>
+    `;
+
+    const style = document.createElement('style');
+    style.textContent = `
+      .box {
+        width: 200px;
+        height: 100px;
+        background-color: green;
+        color: white;
+        text-align: center;
+        padding: 20px;
+      }
+    `;
+    document.head.appendChild(style);
+  }
+}
+
+customElements.define('gree-box', GreenBox);
