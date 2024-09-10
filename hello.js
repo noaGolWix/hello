4548cc75379b64aaa7d0f9a6aced9a396bd8ae73
noaGolWix <157351223+noaGolWix@users.noreply.github.com>
2024-09-10 13:43:59 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
 class OneDialog extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<html>
-<head>
-  <title>Red Box with Text</title>
-  <style>
-    .red-box {
-      width: 200px;
-      height: 100px;
-      background-color: red;
-      color: white;
-      text-align: center;
-      padding: 20px;
-    }
-  </style>
-</head>
-<body>
-  <div id="box"></div>
-  <script>
-    const box = document.getElementById('box');
-    box.classList.add('red-box');
-    box.textContent = 'Hi Noa';
-  </script>
-</body>
-</html>`;
+    this.innerHTML = `
+      <div id="box" class="red-box">Hi Noa</div>
+    `;
   }
 }
+
 customElements.define('one-dialog', OneDialog);
